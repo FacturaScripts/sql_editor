c894780034dab093f025bb14d84da6e1882cc5f7
NeoRazorX <neorazorx@gmail.com>
2018-11-08 17:40:38 +0100

    Mejorado el código y actualizada la versión.

--- controller/sql_editor.php
+++ controller/sql_editor.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  * This file is part of FacturaSctipts
  * Copyright (C) 2015-2016    Carlos Garcia Gomez         neorazorx@gmail.com
@@ -25,65 +24,65 @@
  */
 class sql_editor extends fs_controller
 {
-   public $resultados;
-   public $sentencia;
-   
-   public function __construct()
-   {
-      parent::__construct(__CLASS__, 'SQL Editor', 'admin');
-   }
-   
-   protected function private_core()
-   {
-      $this->resultados = array();
-      $this->sentencia = '';
-      
-      if( isset($_POST['sql']) )
-      {
-         $this->sentencia = $_POST['sql'];
-         
-         if($this->sentencia == '')
-         {
-            
-         }
-         else if(!$this->user->admin)
-         {
+
+    /**
+     *
+     * @var array
+     */
+    public $resultados;
+
+    /**
+     *
+     * @var string
+     */
+    public $sentencia;
+
+    public function __construct()
+    {
+        parent::__construct(__CLASS__, 'SQL Editor', 'admin');
+    }
+
+    public function columnas_resultado()
+    {
+        return empty($this->resultados) ? [] : array_keys($this->resultados[0]);
+    }
+
+    /**
+     * 
+     * @param string $sql
+     */
+    protected function ejecutar($sql)
+    {
+        if (substr(strtolower($sql), 0, 6) == 'select') {
+            $this->resultados = $this->db->select($sql);
+            $this->new_message('Secuencia ejecutada.');
+        } else if (substr(strtolower($sql), 0, 4) == 'show') {
+            $this->resultados = $this->db->select($sql);
+            $this->new_message('Secuencia ejecutada.');
+        } else if (substr(strtolower($sql), 0, 8) == 'describe') {
+            $this->resultados = $this->db->select($sql);
+            $this->new_message('Secuencia ejecutada.');
+        } elseif ($this->db->exec($sql)) {
+            $this->new_message('Secuencia ejecutada correctamente.');
+        } else {
+            $this->new_error_msg('Error al ejercutar la secuencia.');
+        }
+    }
+
+    protected function private_core()
+    {
+        $this->resultados = [];
+        $this->sentencia = isset($_POST['sql']) ? $_POST['sql'] : '';
+
+        if (empty($this->sentencia)) {
+            return;
+        }
+
+        if (!$this->user->admin) {
             $this->new_error_msg('Solamente un administrador puede usar esta página.');
-         }
-         else if( substr( strtolower($this->sentencia), 0, 6 ) == 'select' )
-         {
-            $this->resultados = $this->db->select($this->sentencia);
-            $this->new_message('Secuencia ejecutada.');
-         }
-         else if( substr( strtolower($this->sentencia), 0, 4 ) == 'show' )
-         {
-            $this->resultados = $this->db->select($this->sentencia);
-            $this->new_message('Secuencia ejecutada.');
-         }
-         else if( substr( strtolower($this->sentencia), 0, 8 ) == 'describe' )
-         {
-            $this->resultados = $this->db->select($this->sentencia);
-            $this->new_message('Secuencia ejecutada.');
-         }
-         else
-         {
-            if( $this->db->exec($this->sentencia) )
-            {
-               $this->new_message('Secuencia ejecutada correctamente.');
-            }
-            else
-               $this->new_error_msg('Error al ejercutar la secuencia.');
-         }
-      }
-   }
-   
-   public function columnas_resultado()
-   {
-      if($this->resultados)
-      {
-         return array_keys($this->resultados[0]);
-      }
-      else
-         return array();
-   }
+            return;
+        }
+
+        $this->ejecutar($this->sentencia);
+    }
 }
